21d26b0ab566f3785e65daf9b1fdabe618520b7a
Coinmaster <70707774+Coinmaster123456@users.noreply.github.com>
2021-12-25 02:40:19 +0100

    Finished implementing basic functionality

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const adminkey = 'ZL0j7LniNCwqmR13WlwO'
 const tokenfreq = 60 //in sec
 const randomfreq = 10 //in sec
 const cleardatabase = true //clears whole database after server start ONLY USEFUL FOR DEVELOPMENT
+const serverversion = 1
+//make const classes instance
 
 initialize()
 var db = new loki('tokens.db', { autoload: true, autosave: true, autoloadCallback: databaseInitialize })
@@ -35,8 +37,9 @@ app.post('/add', async (req, res) => {
     res.status(400).send({"error-code":400,"error-message":"Invalid website","data":[]})
     return
   }
-  db.getCollection('classes').insert({'name': name, 'url': url, 'id': uuidv4(), 'tokens':[]})
-  res.status(200).send({"error-code":200,"error-message":"OK","data":[]})
+  var id = uuidv4()
+  db.getCollection('classes').insert({'name': name, 'url': url[0], 'id': id, 'tokens':[]})
+  res.status(200).send({"error-code":200,"error-message":"OK","data":[{'name': name, 'url': url[0], 'id': id}]})
 })
 
 app.post('/remove/*', (req, res) => {
@@ -52,9 +55,18 @@ app.post('/*/add', (req, res) => {
   res.status(200).send({"error-code":200,"error-message":"OK","data":[]})
 })
 
+app.get('/', (req, res) => {
+  res.status(200).send({"error-code":200,"error-message":"OK","data":[{'serverversion':serverversion}]})
+})
+
 app.get('/*', (req, res) => {
-  var collection = db.getCollection(req.path.replaceAll('/','')) //Partially right
-  res.status(200).send({"error-code":200,"error-message":"OK","data":[]})
+  var schoolClass = db.getCollection('classes').findOne({'id':req.path.replaceAll('/','')})
+  if (schoolClass === null)
+  {
+    res.status(404).send({"error-code":404,"error-message":"Not found","data":[]})
+    return
+  }
+  res.status(200).send({"error-code":200,"error-message":"OK","data":[schoolClass]})
 })
 
 function initialize() {
