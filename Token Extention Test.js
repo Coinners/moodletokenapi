4b4d17b40d567a58aa7a58f86cfea20fc0449adf
Coinmaster <70707774+Coinmaster123456@users.noreply.github.com>
2021-12-27 02:32:20 +0100

    Added new methods for extension

--- Token Extention Test.js
+++ Token Extention Test.js
@@ -12,9 +12,13 @@ var client = got.extend({ cookieJar })
 var login = await client.get('https://moodle.rbs-ulm.de/moodle/').text()
 var userid = login.match(/(?<=php\?userid=)\d+/)[0]
 var sessionkey = login.match(/(?<=sesskey=)\w{10}/)[0]
+var username = login.match(/(?<=class="usertext mr-1">).*?(?=\<\/span)/)[0]
+console.log('sessionkey='+sessionkey+'\nuserid='+userid+'\nusername='+username)
 while (true) {
     await new Promise(r => setTimeout(r, 60*1000));
-    var req = await client.get('https://moodle.rbs-ulm.de/moodle/login/index.php?testsession='+userid)
+    //var req = await client.get('https://moodle.rbs-ulm.de/moodle/login/index.php?testsession='+userid)
+    //var req = await client.get('https://moodle.rbs-ulm.de/moodle/')
+    var req = await client.post('https://moodle.rbs-ulm.de/moodle/lib/ajax/service.php?sesskey='+sessionkey+'&info=core_session_touch',{json:[{"index":0,"methodname":"core_session_touch","args":{}}]})
     console.log(req.statusCode)
     console.log(cookieJar.getCookies('https://moodle.rbs-ulm.de'))
 }
