e332a643de0465710ed7a37c89fed4088bea73f8
Coinmaster <70707774+Coinmaster123456@users.noreply.github.com>
2021-12-27 13:04:05 +0100

    Update Token Extention Test.js

--- Token Extention Test.js
+++ Token Extention Test.js
@@ -2,7 +2,7 @@ import {promisify} from 'util'
 import got from 'got'
 import {CookieJar} from 'tough-cookie'
 
-const sessiontoken = '8a76vmeqvpl1ihhhf92uqm24b8'
+const sessiontoken = 'fvdi07rkq5miv3vmc1p8j7llq3'
 const cookieJar = new CookieJar()
 const setCookie = promisify(cookieJar.setCookie.bind(cookieJar))
 
@@ -13,12 +13,15 @@ var login = await client.get('https://moodle.rbs-ulm.de/moodle/').text()
 var userid = login.match(/(?<=php\?userid=)\d+/)[0]
 var sessionkey = login.match(/(?<=sesskey=)\w{10}/)[0]
 var username = login.match(/(?<=class="usertext mr-1">).*?(?=\<\/span)/)[0]
-console.log('sessionkey='+sessionkey+'\nuserid='+userid+'\nusername='+username)
+console.log('time='+Date.now()+'+\nsessionkey='+sessionkey+'\nuserid='+userid+'\nusername='+username)
 while (true) {
-    await new Promise(r => setTimeout(r, 60*1000));
-    //var req = await client.get('https://moodle.rbs-ulm.de/moodle/login/index.php?testsession='+userid)
+    var timeleft = await client.post('https://moodle.rbs-ulm.de/moodle/lib/ajax/service.php?sesskey='+sessionkey+'&info=core_session_time_remaining&nosessionupdate=true',{json:[{"index":0,"methodname":"core_session_time_remaining","args":{}}]}).json()
+    timeleft = timeleft[0]['data']['timeremaining']
+    await new Promise(r => setTimeout(r, (timeleft-2)*1000));
+    var req = await client.get('https://moodle.rbs-ulm.de/moodle/login/index.php?testsession='+userid)
     //var req = await client.get('https://moodle.rbs-ulm.de/moodle/')
-    var req = await client.post('https://moodle.rbs-ulm.de/moodle/lib/ajax/service.php?sesskey='+sessionkey+'&info=core_session_touch',{json:[{"index":0,"methodname":"core_session_touch","args":{}}]})
+    //var req = await client.post('https://moodle.rbs-ulm.de/moodle/lib/ajax/service.php?sesskey='+sessionkey+'&info=core_session_touch',{json:[{"index":0,"methodname":"core_session_touch","args":{}}]})
+    console.log(Date.now())
     console.log(req.statusCode)
     console.log(cookieJar.getCookies('https://moodle.rbs-ulm.de'))
 }
