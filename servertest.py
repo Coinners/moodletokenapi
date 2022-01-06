d871b1144572625b4f63b5fdea881a1776bbb7ab
Coinmaster <70707774+Coinmaster123456@users.noreply.github.com>
2022-01-06 13:52:26 +0100

    Delete servertest.py

--- servertest.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-adminkey = "ZL0j7LniNCwqmR13WlwO"
-
-schoolClass = requests.post('http://localhost:3000/add',json={"key":adminkey,"name":"6TG10/1 Ulm","url":"https://moodle.rbs-ulm.de/moodle/mod/forum/view.php?id=21033"})
-print(schoolClass.text)
-addUser = requests.post('http://localhost:3000/'+schoolClass.json()['data']['id']+'/add',json={"name":"jungnicl","password":"GGAnderson","token":"t225mqv6di3ned9grj0c2n3lit"})
-print(addUser.text)
